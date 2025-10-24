2ffe06ad8a0ae6f74e35101502bba041df9927a8
iankendoit <iankencruz@gmail.com>
2025-10-24 14:31:12 +1100

    update workflows

--- tsup.config.js
+++ tsup.config.js
@@ -1,56 +1,64 @@
 // tsup.config.js
 export default {
-  entry: ['src/index.jsx'],
-  format: ['esm', 'cjs'],
-  dts: false,                 // set to true if you switch to TypeScript
+  entry: ["src/index.jsx"],
+  format: ["esm", "cjs"],
+  dts: false, // set to true if you switch to TypeScript
   clean: true,
   splitting: false,
   sourcemap: true,
   minify: true,
   external: [
     // Core React
-    'react', 
-    'react-dom',
-    
+    "react",
+    "react-dom",
+
     // Utilities
-    'react-hot-toast',
-    'dayjs',
-    'dayjs/locale/de',
-    'dompurify',
-    
+    "react-hot-toast",
+    "dayjs",
+    "dayjs/locale/de",
+    "dompurify",
+
     // Radix UI - all components
-    '@radix-ui/react-label',
-    '@radix-ui/react-popover',
-    '@radix-ui/react-radio-group',
-    '@radix-ui/react-select',
-    '@radix-ui/react-separator',
-    
+    "@radix-ui/react-label",
+    "@radix-ui/react-popover",
+    "@radix-ui/react-radio-group",
+    "@radix-ui/react-select",
+    "@radix-ui/react-separator",
+
     // Date picker
-    'react-day-picker',
-    
+    "react-day-picker",
+
     // MUI - Core and all date picker components
-    '@mui/material',
-    '@mui/x-date-pickers',
-    '@mui/x-date-pickers/LocalizationProvider',
-    '@mui/x-date-pickers/AdapterDayjs',
-    '@mui/x-date-pickers/DateTimeField',
-    
+    "@mui/material",
+    "@mui/x-date-pickers",
+    "@mui/x-date-pickers/LocalizationProvider",
+    "@mui/x-date-pickers/AdapterDayjs",
+    "@mui/x-date-pickers/DateTimeField",
+
     // Emotion
-    '@emotion/react',
-    '@emotion/styled',
-    
+    "@emotion/react",
+    "@emotion/styled",
+
     // React Select
-    'react-select',
-    'react-select/animated',
-    
+    "react-select",
+    "react-select/animated",
+
     // Heroicons
-    '@heroicons/react',
-    '@heroicons/react/20/solid',
-    '@heroicons/react/24/outline'
+    "@heroicons/react",
+    "@heroicons/react/20/solid",
+    "@heroicons/react/24/outline",
   ], // keep as peers
-  jsx: 'automatic',           // transforms JSX with the new runtime
+  jsx: "automatic", // transforms JSX with the new runtime
   esbuildOptions: (options) => {
-    options.jsx = 'automatic'
-    options.jsxImportSource = 'react'
-  }
-}
+    options.jsx = "automatic";
+    options.jsxImportSource = "react";
+  },
+  // 🔒 Guarantees filenames match package.json
+  outExtension: (ctx) => ({
+    js: ctx.format === "cjs" ? ".cjs" : ".js",
+  }),
+  // Sensible output target for libs consumed by modern bundlers
+  target: "es2019",
+  treeshake: true,
+};
+
