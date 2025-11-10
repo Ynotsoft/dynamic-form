6ee91c0c69ccdcdaf3438cbcc877acbf687e9bdf
ikruz <iankencruz@gmail.com>
2025-11-10 11:49:49 +1100

    add tailwind cli and update version package

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,14 @@
+import "./tailwind.css";
 
-export { default as DynamicForm } from './DynamicForm/DynamicForm.jsx';
+export { default as DynamicForm } from "./DynamicForm/DynamicForm.jsx";
 
 // Default exports for backward compatibility
 
-import DynamicFormComponent from './DynamicForm/DynamicForm.jsx';
+import DynamicFormComponent from "./DynamicForm/DynamicForm.jsx";
 
 // Export default as DynamicForm (main component)
 export default DynamicFormComponent;
 
 // Also export a default MyButton for direct import
 export { DynamicFormComponent };
+
